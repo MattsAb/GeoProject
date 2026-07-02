1fc1b283024eab97fb1eedc3fe356455d942a67f
MattAb <matt0abb@gmail.com>
2026-07-02 18:28:43 +0300

    feat: add get post route

--- apps/api/src/controllers/postController.ts
+++ apps/api/src/controllers/postController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express'
 import { prisma } from '../config/prisma'
+import { ServerError } from '../middleware/errorMiddleware';
 
 export async function createPost(req: Request, res: Response) {
 
@@ -16,11 +17,34 @@ export async function createPost(req: Request, res: Response) {
         }
     })
 
-    return res.status(201).json({ success: true, post })
+    return res.status(201).json({ success: true, post });
 
 }
 
-export async function getPost(req: Request, res: Response) {}
+export async function getPost(req: Request, res: Response) {
+    const postId = req.body.postId;
+
+    if (!postId) {throw new ServerError(404, 'Post not found');}
+
+    const post = await prisma.post.findUnique({
+        where: {id: postId},
+        include: {
+            user: true,
+            comments: {
+                include: {
+                    user: {
+                        select: { id: true, username: true, avatarUrl: true }
+                    }
+                }
+            },
+            _count: {
+                select: {likes: true, comments: true}
+            }
+        }
+    })
+
+    return res.status(200).json({success: true, post});
+}
 
 export async function editPost(req: Request, res: Response) {}
 
