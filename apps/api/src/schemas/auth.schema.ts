9eda225ad9f3319db5662b81c501ea295a940f70
MattAb <matt0abb@gmail.com>
2026-08-31 15:49:54 +0300

    fix: fix signup schema

--- apps/api/src/schemas/auth.schema.ts
+++ apps/api/src/schemas/auth.schema.ts
@@ -8,7 +8,13 @@ export const signupSchema = z.object({
       .min(5, {error: "username must be atleast 5 charcters long"})
       .max(30, {error: "username must be shorter than 30 chracaters long"})
       .regex(/^[a-zA-Z0-9_]+$/, {error: 'Alphanumeric and underscores only'}),
-    password: z.string().min(8 , {error: 'password must be atleast 8 characters long'}),
+    password: z
+    .string()
+    .min(8, { error: 'Password must be at least 8 characters' })
+    .regex(/[A-Z]/, { error: 'Password must contain at least one uppercase letter' })
+    .regex(/[a-z]/, { error: 'Password must contain at least one lowercase letter' })
+    .regex(/[0-9]/, { error: 'Password must contain at least one number' })
+    .regex(/[^A-Za-z0-9]/, { error: 'Password must contain at least one special character' }),
   }),
 });
 
